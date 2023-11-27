e1ea9f9eb8859d752a99dcec946ca351a17daac8
Jeff Ripke <jripke74@gmail.com>
2023-11-27 07:41:47 -0800

    add UsersList.js jsx

--- frontend/src/user/pages/Users.js
+++ frontend/src/user/pages/Users.js
@@ -7,7 +7,8 @@ const Users = () => {
     {
       id: "u1",
       name: "Jeff Allen",
-      image: "http://klkjl",
+      image:
+        "https://images.unsplash.com/photo-1490730141103-6cac27aaab94?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJlZXxlbnwwfHwwfHx8MA%3D%3D://klkjl",
       places: 3,
     },
   ];
