a1f6567fa9130f61a42aa9f893f5864f6771592f
Jeff Ripke <jripke74@gmail.com>
2023-12-01 09:55:23 -0800

    add auth login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./user/pages/Auth";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 const App = () => {
@@ -30,6 +31,9 @@ const App = () => {
           <Route path="/places/:placeId" exact>
             <UpdatePlace />
           </Route>
+          <Router path="/auth/">
+            <Auth />
+          </Router>
           <Redirect to="/" />
         </Switch>
       </main>
