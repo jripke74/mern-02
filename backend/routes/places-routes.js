12d5afcb7c85ebc5ac342ce95459af751af36aa2
Jeff Ripke <jripke74@gmail.com>
2023-12-04 15:28:23 -0800

    validating place inputs

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const HttpError = require("../models/http-error");
 
@@ -10,7 +11,15 @@ router.get("/:pid", placesControllers.getPlaceById);
 
 router.get("/user/:uid", placesControllers.getPlacesByUserId);
 
-router.post("/", placesControllers.createPlace);
+router.post(
+  "/",
+  [
+    check("title").not().isEmpty(),
+    check("description").isLength({ min: 5 }),
+    check("address").not().isEmpty(),
+  ],
+  placesControllers.createPlace
+);
 
 router.patch("/:pid", placesControllers.updatePlace);
 
