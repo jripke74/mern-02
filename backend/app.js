992915edc2d100cc3f258d687f9e333baa24a6aa
Jeff Ripke <jripke74@gmail.com>
2023-12-16 15:10:32 -0800

    add nodemon.json

--- backend/app.js
+++ backend/app.js
@@ -49,7 +49,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://${mongoDbUsername}:${mongoDbPassword}@cluster0.mxuwfw1.mongodb.net/mern?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${mongoDbPassword}@cluster0.mxuwfw1.mongodb.net/mern?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(5003);
